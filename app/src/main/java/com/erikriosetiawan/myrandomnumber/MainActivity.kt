d5973c5571440808f590210d0ca8c766121c2911
erikrios <erikriosetiawan49@gmail.com>
2020-03-13 19:20:56 +0700

    Add view model and live data

--- app/src/main/java/com/erikriosetiawan/myrandomnumber/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/myrandomnumber/MainActivity.kt
@@ -2,16 +2,28 @@ package com.erikriosetiawan.myrandomnumber
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.erikriosetiawan.myrandomnumber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.randomNumber.observe(this, Observer {
+            binding.textViewNumber.text = it
+        })
+
+        binding.floatingActionButton.setOnClickListener {
+            viewModel.createRandomNumber()
+        }
     }
 }
